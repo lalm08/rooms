2a90700ed80dc7e4dd713b89d52d10bb1deb0bd2
lalm08 <ekaterinka3733@gmail.com>
2026-01-01 22:35:06 +0300

    proverka2

--- backend/prisma.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig, env } from 'prisma/config'
-
-export default defineConfig({
-  schema: './prisma/schema.prisma',
-  datasource: {
-    // В Prisma 7 env используется как функция: env("ИМЯ_ПЕРЕМЕННОЙ")
-    url: process.env.DATABASE_URL || "postgresql://dummy:dummy@localhost:5432/db",
-  },
-})
